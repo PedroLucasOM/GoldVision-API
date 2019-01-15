1b043d7e024a36809ffce65498220cd3c5cf4213
Pedro Lucas <pedro99lucasom@gmail.com>
2019-01-15 19:52:04 -0300

    Criação do projeto do curso

---
+++ src/test/java/com/system/algamoney/AlgamoneyapiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.system.algamoney;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlgamoneyapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
