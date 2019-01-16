dc99c61a9ff5f962d89aba2ff03aeda7596d18ae
Pedro Lucas <pedro99lucasom@gmail.com>
2019-01-15 21:54:11 -0300

    Implemetanção do evento para adicionar o header location

---
+++ src/main/java/com/system/algamoney/event/listener/RecursoCriadoListener.java
@@ -0,0 +1,27 @@
+package com.system.algamoney.event.listener;
+
+import java.net.URI;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.system.algamoney.event.RecursoCriadoEvent;
+
+public class RecursoCriadoListener implements ApplicationListener<RecursoCriadoEvent> {
+
+	@Override
+	public void onApplicationEvent(RecursoCriadoEvent event) {
+		HttpServletResponse response = event.getResponse();
+		Long codigo = event.getCodigo();
+		
+		adicionarHeaderLocation(response, codigo);
+	}
+	
+	private void adicionarHeaderLocation(HttpServletResponse response, Long codigo) {
+		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/{codigo}").buildAndExpand(codigo).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+	}
+
+}
