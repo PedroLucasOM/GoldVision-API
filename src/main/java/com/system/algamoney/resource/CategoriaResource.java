0af22d308643fcb61e8444dff58c5cbc1b08955b
Pedro Lucas <pedro99lucasom@gmail.com>
2019-01-15 23:28:42 -0300

    Implementação do tratamento de erro para a idempotencia do método delete

--- src/main/java/com/system/algamoney/resource/CategoriaResource.java
+++ src/main/java/com/system/algamoney/resource/CategoriaResource.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.system.algamoney.event.RecursoCriadoEvent;
@@ -46,5 +48,11 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> listarPorId(@PathVariable Long codigo){
 		Categoria categoria = repository.findOne(codigo);
 		return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void excluir(@PathVariable Long codigo) {
+		repository.delete(codigo);
 	}
 }
