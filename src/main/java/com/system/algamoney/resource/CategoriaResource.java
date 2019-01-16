296d27c65d31852d70c5d382bd1555b0d341c591
Pedro Lucas <pedro99lucasom@gmail.com>
2019-01-15 21:54:39 -0300

    Começo da implementação do crud de categoria

---
+++ src/main/java/com/system/algamoney/resource/CategoriaResource.java
@@ -0,0 +1,49 @@
+package com.system.algamoney.resource;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.system.algamoney.event.RecursoCriadoEvent;
+import com.system.algamoney.model.Categoria;
+import com.system.algamoney.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+	@Autowired
+	private CategoriaRepository repository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
+	@GetMapping
+	public List<Categoria> listar(){
+		return repository.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> salvar(@RequestBody Categoria categoria, HttpServletResponse response){
+		Categoria categoriaSalva = repository.save(categoria);
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Categoria> listarPorId(@PathVariable Long codigo){
+		Categoria categoria = repository.findOne(codigo);
+		return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
+	}
+}
