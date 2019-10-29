edd3ee37375557e3b9a65dbb09b8c37c9870a451
Pedro Lucas <pedro99lucasom@gmail.com>
2019-10-29 02:39:55 -0300

    Configuração do CORS

--- src/main/java/com/system/algamoney/cors/CorsFilter.java
+++ src/main/java/com/system/algamoney/cors/CorsFilter.java
@@ -12,44 +12,47 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 
 import com.system.algamoney.config.property.AlgamoneyProperty;
 
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter{
 
 	@Autowired
 	private AlgamoneyProperty property;
 	
 	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
 			throws IOException, ServletException {
-		HttpServletRequest req = (HttpServletRequest) request;
-		HttpServletResponse resp = (HttpServletResponse) response;
 		
-		resp.setHeader("Access-Control-Allow-Origin", property.getOriginPermitida());
-		resp.setHeader("Access-Control-Allow-Credentials", "true");
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) resp;
 		
-		if("OPTIONS".equals(req.getMethod()) && property.getOriginPermitida().equals(req.getHeader("Origin"))) {
-			resp.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-			resp.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
-			resp.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Origin", property.getOriginPermitida());
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+		
+		if ("OPTIONS".equals(request.getMethod()) && property.getOriginPermitida().equals(request.getHeader("Origin"))) {
+			response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
+        	response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+        	response.setHeader("Access-Control-Max-Age", "3600");
 			
-			resp.setStatus(HttpServletResponse.SC_OK);
-		}else {
+			response.setStatus(HttpServletResponse.SC_OK);
+		} else {
 			chain.doFilter(req, resp);
 		}
+		
 	}
 	
 	@Override
-	public void init(FilterConfig filterConfig) throws ServletException {
-		// TODO Auto-generated method stub
-		
+	public void destroy() {
 	}
 
 	@Override
-	public void destroy() {
-		// TODO Auto-generated method stub
-		
+	public void init(FilterConfig arg0) throws ServletException {
 	}
 
 }
