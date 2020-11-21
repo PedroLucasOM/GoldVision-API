446fda2c3ac48544efa8a2d67e7d245f360d47f5
Pedro Lucas <pedro99lucasom@gmail.com>
2020-11-21 03:09:01 -0300

    Conclusão de configuração do docker e swagger

--- src/main/java/com/system/goldvision/config/SwaggerConfig.java
+++ src/main/java/com/system/goldvision/config/SwaggerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.format.FormatterRegistry;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.validation.MessageCodesResolver;
 import org.springframework.validation.Validator;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.servlet.HandlerExceptionResolver;
@@ -15,15 +16,18 @@ import org.springframework.web.servlet.config.annotation.*;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
+import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
@@ -33,13 +37,32 @@ import java.util.List;
 public class SwaggerConfig extends WebMvcConfigurationSupport {
 
     @Bean
-    public Docket greetingApi() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.system.goldvision")).build()
-                .securitySchemes(Lists.newArrayList(apiKey())).securityContexts(Lists.newArrayList(securityContext()))
-                .apiInfo(metaData());
+    public Docket api() {
+
+        List<ResponseMessage> list = new java.util.ArrayList<>();
+
+        return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.system.goldvision"))
+                .paths(PathSelectors.any()).build().securitySchemes(Collections.singletonList(securitySchema()))
+                .securityContexts(Collections.singletonList(securityContext())).pathMapping("/")
+                .useDefaultResponseMessages(false).apiInfo(apiInfo()).globalResponseMessage(RequestMethod.GET, list)
+                .globalResponseMessage(RequestMethod.POST, list);
+    }
+
+    private OAuth securitySchema() {
+
+        List<AuthorizationScope> authorizationScopeList = new ArrayList();
+        authorizationScopeList.add(new AuthorizationScope("read", "read all"));
+        authorizationScopeList.add(new AuthorizationScope("write", "access all"));
+
+        List<GrantType> grantTypes = new ArrayList();
+        GrantType creGrant = new ResourceOwnerPasswordCredentialsGrant("http://localhost:8080" +"/oauth/token");
+
+        grantTypes.add(creGrant);
+
+        return new OAuth("oauth2schema", authorizationScopeList, grantTypes);
 
     }
+
 
     @Bean
     SecurityContext securityContext() {
@@ -47,20 +70,26 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
     }
 
     List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return Lists.newArrayList(new SecurityReference("JWT", authorizationScopes));
+        final AuthorizationScope[] authorizationScopes = new AuthorizationScope[3];
+        authorizationScopes[0] = new AuthorizationScope("read", "read all");
+        authorizationScopes[1] = new AuthorizationScope("trust", "trust all");
+        authorizationScopes[2] = new AuthorizationScope("write", "write all");
+        return Collections.singletonList(new SecurityReference("oauth2schema", authorizationScopes));
+    }
+
+    @Bean
+    public SecurityConfiguration securityInfo() {
+        return new SecurityConfiguration("angular", "@ngul@r0", "", "", "", ApiKeyVehicle.HEADER, "", " ");
     }
 
     private ApiKey apiKey() {
         return new ApiKey("JWT", "Authorization", "header");
     }
 
-    private ApiInfo metaData() {
+    private ApiInfo apiInfo() {
         return new ApiInfoBuilder().title("GoldVision API").description("\"Documentação da API GoldVision\"")
-                .version("1.0.0").license("Apache License Version 2.0")
-                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"").build();
+                .contact(new Contact("Pedro Lucas", "https://github.com/PedroLucasOM", "pedro99lucasom@gmail.com"))
+                .license("Open Source").version("1.0.0").build();
     }
 
     @Bean
